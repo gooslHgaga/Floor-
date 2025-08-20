a985c75fc29ec4abb095f1f894c9267e1da049bc
gooslHgaga <shaheenahmad953@gmail.com>
2025-08-20 23:52:18 +0300

    audio_service.dart

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -1,26 +1,15 @@
-// lib/core/services/audio_service.dart
-// نسخة معدّلة: اختيار ملف صوت من الهاتف فقط (بدون أذان افتراضي)
-
-import 'package:flutter/material.dart';
+// بدون صوت افتراضي، فقط اختيار ملف من الهاتف
 import 'package:file_picker/file_picker.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 final AudioPlayer _player = AudioPlayer();
 
 class AudioService {
-  /// لا يُشغَّل أذان افتراضي
   static Future<void> playAdan() async {}
-
-  /// اختيار ملف صوت من الهاتف وتشغيله
   static Future<void> pickExternal() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.audio,
-      allowCompression: false,
-    );
-
-    if (result != null && result.files.single.path != null) {
-      await _player.stop(); // إيقاف أي تشغيل سابق
-      await _player.play(DeviceFileSource(result.files.single.path!));
+    final res = await FilePicker.platform.pickFiles(type: FileType.audio);
+    if (res != null && res.files.single.path != null) {
+      await _player.play(DeviceFileSource(res.files.single.path!));
     }
   }
 }
