d2cbcbb23eb5db94ced3a7eebf9592d05f013f0f
gooslHgaga <shaheenahmad953@gmail.com>
2025-08-20 23:14:14 +0300

    hijri_helper.dart

--- lib/core/utils/hijri_helper.dart
+++ lib/core/utils/hijri_helper.dart
@@ -1,7 +1,6 @@
 import 'package:jhijri/jhijri.dart';
 
 String getTodayHijri() {
-  final today = DateTime.now();
-  final h = JHijri(fDate: today);
+  final h = JHijri(fDate: DateTime.now());
   return '${h.day} ${h.longMonthName} ${h.year} هـ';
 }
