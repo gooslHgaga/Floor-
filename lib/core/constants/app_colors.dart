0f383457a3e4162ec4cb9c617d305752ce064185
gooslHgaga <shaheenahmad953@gmail.com>
2025-08-22 22:32:05 +0300

    Delete lib/core directory

--- lib/core/constants/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary   = Color(0xff1e3a8a);
-  static const Color secondary = Color(0xff34d399);
-  static const Color surface   = Color(0xff111827);
-}
--- lib/core/utils/hijri_helper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hijri/hijri.dart';
-
-String getTodayHijri() {
-  final h = HijriCalendar.now();
-  return '${h.hDay} ${h.longMonthName} ${h.hYear} هـ';
-}
